0ba53b425083a789dfe88900e5f41df372426501
Edwin Villanueva <edwincv0@gmail.com>
2013-07-21 20:39:59 +0000

    Updated stuff

--- web.js
+++ web.js
@@ -4,9 +4,12 @@ var app = express.createServer(express.logger());
 
 var buf = fs.readFileSync("index.html");
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
   response.send(buf.toString());
 });
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
